0dc89d585df4560498d141b9bff95bc79fb15f1e
zickieloox <zickieloox@gmail.com>
2016-12-15 15:16:14 +0700

    delete unused features and add change PKG Name, app label functions

--- src/com/softsec/editor/Main.java
+++ src/com/softsec/editor/Main.java
@@ -6,28 +6,23 @@ import java.io.FileOutputStream;
 import com.softsec.editor.decode.AXMLDoc;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void change(String inputFile, String outputFile, String newPackageName, String newAppLabel) {
 		try{
 			AXMLDoc doc = new AXMLDoc();
-			doc.parse(new FileInputStream(args[0]));
-//			doc.testSize();
-//			doc.print();
-//			for(int i=0;i<doc.getResBlock().getResourceIds().size();i++)//测试代码 
-//				System.out.println(i+": "+doc.getResBlock().getResourceIdAt(i));
-			
-			ChannelEditor editor = new ChannelEditor(doc);
-			editor.commit();
-			doc.build(new FileOutputStream(args[1]));
+			doc.parse(new FileInputStream(inputFile));
+			doc.print();
 
-//			AXMLDoc doc2 = new AXMLDoc();
-//			doc2.parse(new FileInputStream(args[1]));
-//			doc2.testSize();
-//			doc2.print();
-//		for(int i=0;i<doc2.getResBlock().getResourceIds().size();i++)//测试代码 
-//				System.out.println(i+": "+doc2.getResBlock().getResourceIdAt(i));
+			ChannelEditor editor = new ChannelEditor(doc);
+			editor.commit(newPackageName, newAppLabel);
+			doc.print();
+			doc.build(new FileOutputStream(outputFile));
 
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+	}
+
+	public static void main(String[] args) {
+		change("/sdcard/z.xml", "/sdcard/z.xml", "com.zic.test", "");
 	}
 }
